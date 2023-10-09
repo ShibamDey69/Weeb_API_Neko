24f92a4006ad9ef77f0d2d8839da80a906c5a0bc
NekoSenpai69 <123672103+NekoSenpai69@users.noreply.github.com>
2023-10-09 18:12:43 +0000

    Little

--- routes/nsfw_router.js
+++ routes/nsfw_router.js
@@ -5,11 +5,11 @@ import { xvideosSingle,xvideosMultiple } from '../functions/nsfw/porn/xvideos.js
 const router = express.Router()
 
 /** XNXX API Route */
-router.route("porn/xnxx/multiple").get(xnxxMultiple)
-router.route("porn/xnxx/single").get(xnxxSingle)
+router.route("/porn/xnxx/multiple").get(xnxxMultiple)
+router.route("/porn/xnxx/single").get(xnxxSingle)
 
 /** XVIDEOS API Route */
-router.route("porn/xvideos/multiple").get(xvideosMultiple)
-router.route("porn/xvideos/single").get(xvideosSingle)
+router.route("/porn/xvideos/multiple").get(xvideosMultiple)
+router.route("/porn/xvideos/single").get(xvideosSingle)
 
 export default router
