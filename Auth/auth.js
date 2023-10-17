8c95485e56898e58350c01e49b156e4464eb7601
NekoSenpai69 <123672103+NekoSenpai69@users.noreply.github.com>
2023-10-17 12:25:58 +0000

    Auth successful!!

--- Auth/auth.js
+++ Auth/auth.js
@@ -1,11 +1,10 @@
 import fs from 'node:fs';
-let folder = "./storage"
 let path = "./storage/user.json"
-
+let arr = []
 export function authenticateKey (req, res, next) {
-  let MAX = 5000
+  let MAX = 50
   let users = JSON.parse(fs.readFileSync(path,'utf8'));
-  
+  arr.push(...users)
   let API_KEY = req.query.api_key; //Add API key to headers
   let account = users.find((user) => user.API_KEY == API_KEY);
   // find() returns an object or undefined
@@ -14,8 +13,10 @@ export function authenticateKey (req, res, next) {
     //check the number of times the API has been used in a particular day
     let today = new Date().toISOString().split("T")[0];
     let usageCount = account.usage.findIndex((day) => day.date == today);
+    if(account.admin === false) {
     if (usageCount >= 0) {
       //If API is already used today
+      
       if (account.usage[usageCount].count >= MAX) {
         //stop if the usage exceeds max API calls
         res.status(429).send({
@@ -27,15 +28,42 @@ export function authenticateKey (req, res, next) {
       } else {
         //have not hit todays max usage
         account.usage[usageCount].count++;
-        console.log(account.usage);
+        arr.push(account);
+        const ids = arr.map(({ API_KEY }) => API_KEY);
+const filtered = arr.filter(({ API_KEY }, index) => !ids.includes(API_KEY, index + 1));
+        fs.writeFileSync(path, JSON.stringify(filtered, null, 2));
         next();
       }
     } else {
       //Push todays's date and counting: 1 if there is a past date
       account.usage.push({ date: today, count: 1 });
+      arr.push(account);
+        const ids = arr.map(({ API_KEY }) => API_KEY);
+const filtered = arr.filter(({ API_KEY }, index) => !ids.includes(API_KEY, index + 1));
+        fs.writeFileSync(path, JSON.stringify(filtered, null, 2));
       //ok to use again
       next();
     }
+    } else {
+      if (usageCount >= 0) {
+        account.usage[usageCount].count++;
+        arr.push(account);
+        const ids = arr.map(({ API_KEY }) => API_KEY);
+const filtered = arr.filter(({ API_KEY }, index) => !ids.includes(API_KEY, index + 1));
+        fs.writeFileSync(path, JSON.stringify(filtered, null, 2));
+        next();
+      
+    } else {
+      //Push todays's date and counting: 1 if there is a past date
+      account.usage.push({ date: today, count: 1 });
+      arr.push(account);
+        const ids = arr.map(({ API_KEY }) => API_KEY);
+const filtered = arr.filter(({ API_KEY }, index) => !ids.includes(API_KEY, index + 1));
+        fs.writeFileSync(path, JSON.stringify(filtered, null, 2));
+      //ok to use again
+      next();
+    }
+   } 
   } else {
     //Reject request if API key doesn't match
     res.status(403).send({ error: { code: 403, msg: "You not allowed." } });
--- routes/router.js
+++ routes/router.js
@@ -13,12 +13,14 @@ function API(len = 16) {
   let id = Date.now();
 
 router.route("/register").post((req,res) => {
+  try{
   let users = JSON.parse(fs.readFileSync(path,'utf8'));
   if(!fs.existsSync(folder)) {
     fs.mkdirSync(folder)
   }
   arr.push(...users)
   let API_KEY = API(25)
+  let admin = false
   let today = new Date().toISOString().split('T')[0];
   let { name, email, password } = req.body
   if (!name || !email || !password) {
@@ -26,34 +28,47 @@ router.route("/register").post((req,res) => {
       error: "Please fill all the fields!!"
     })
   }
-  function User(ids,names,emails,passwords,API_KEYS,Today) {
+    console.log(`lol1`);
+  function User(ids,admins,names,emails,passwords,API_KEYS,Today) {
      this.id = ids;
+     this.admin = admins
      this.name = names;
      this.email = emails;
      this.password = passwords;
      this.API_KEY = API_KEYS;
      this.usage = [{ date: Today, count: 0 }];
   }
-  const user = new User(id,name,email,password,API_KEY,today)
+  const user = new User(id,admin,name,email,password,API_KEY,today,)
   const result = arr.find(({ email }) => email === user.email);
   if(result?.email === user.email) return res.status(400).json({
     msg: "Email already exists!!"
   })
   arr.push(user)
-fs.writeFileSync(path,JSON.stringify([...new Set(arr)]))
-    
+    function removeDuplicates(arr) {
+        return arr.filter((item,
+            index) => arr.indexOf(item) === index);
+    }
+    const unique = arr.filter((obj, index) => {
+          return index === arr.findIndex(o => obj.email === o.email);
+      });
+fs.writeFileSync(path,JSON.stringify(unique,null,2))
   res.send({
     msg:"Successfully Registered!!",
     user:{
     "_id":id,
+    "admin":admin,
     "name":name,
     "email":email,
     "API_KEY":API_KEY
     }
   })
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 router.route("/login").post((req,res) => {
+  try {
   let users = JSON.parse(fs.readFileSync(path,'utf8'));
   let { password } = req.body
   let emails = req.body.email
@@ -74,11 +89,15 @@ router.route("/login").post((req,res) => {
     msg:"Successfully Logged In!!",
     user:{
       "_id":result.id,
+      "admin":result.admin,
       "name":result.name,
       "email":result.email,
       "API_KEY":result.API_KEY
     }
   })
+  } catch(error) {
+    console.log(error); 
+  }
 })
 
 
