89ce19d2eff86aab18fa769165fbccf0127086ea
NekoSenpai69 <123672103+NekoSenpai69@users.noreply.github.com>
2023-11-21 19:43:50 +0000

    Same mistake

--- functions/sfw/anime/gogoanime.js
+++ functions/sfw/anime/gogoanime.js
@@ -61,7 +61,7 @@ export async function gogoanimeEpisode(req, res) {
   try {
     let EpisodeId = req.query.id;
     let server = req.query.server || "gogocdn";
-    if (!anime) return res.status(404).send({
+    if (!animeId) return res.status(404).send({
       status: 404,
       response: "failed!!",
       reason: "Please Provide A EpisodeId...!!"
