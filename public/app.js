2483771260baa9960836c0d3298e8bfd7389e2e7
NekoSenpai69 <123672103+NekoSenpai69@users.noreply.github.com>
2023-11-25 19:40:26 +0000

    Neko

--- public/app.js
+++ public/app.js
@@ -9,7 +9,6 @@ const sign_up_name = document.querySelector(".sign-up-form #username");
 const sign_up_phone = document.querySelector(".sign-up-form #number");
 const sign_in_email = document.querySelector(".sign-in-form #email");
 const sign_in_password = document.querySelector(".sign-in-form #password");
-const urlWow = "https://weebapineko.nekosenpai69.repl.co/register";
 // Example POST method implementation:
 async function postData(url = "") {
   try {
@@ -21,7 +20,6 @@ async function postData(url = "") {
   }
 }
 
-
 sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
 });
@@ -32,13 +30,10 @@ sign_in_btn.addEventListener("click", () => {
 
 sign_up_form.addEventListener("submit", async(e) => {
   e.preventDefault();
-  console.log("ok")
-console.log(`${sign_up_form.action}/${sign_up_email.value}/${sign_up_name.value}/${sign_up_phone.value}/${sign_up_password.value}`)
 postData(`${sign_up_form.action}?email=${sign_up_email.value}&username=${sign_up_name.value}&phone=${sign_up_phone.value}&password=${sign_up_password.value}`).then((data) => {
-  console.log(data)
-  alert(data.message);
+  alert(data.message)
 }).catch(e => {
-    console.log(e.message)
+    alert("error")
 }) ;
   
 });
@@ -46,6 +41,6 @@ postData(`${sign_up_form.action}?email=${sign_up_email.value}&username=${sign_up
 sign_in_form.addEventListener("submit", async(e) => {
   e.preventDefault();
   postData(`${sign_in_form.action}?email=${sign_in_email.value}&password=${sign_in_password.value}`).then((data) => {
-    alert(data.message);
-   }).catch(e => console.log(e.message))
+    alert(data.apikey);
+   }).catch(e => alert(e.message))
 });
